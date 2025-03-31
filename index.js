128444b90a31c98c40b65ff80b109e223f27c2ec
row-huh <rohaabropathan@gmail.com>
2025-03-31 17:22:52 +0500

    implemented playBgMusic + minor fixes

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
+// necessary variables go here
+NOTES = [   // all instances where the player must intervene. 
+    {"time": 1.2, "key": "a", "sound" : "1.mp3"}
+]  
+// NOTES attributes: Time is the time at which they should intervene, key is the action they must take, sound is the sound that will play when the provided action is taken
+
+
+// roha to roha : DO NOT EDIT THE TEXT BELOW THIS - LET THIS BE HERE REPRESENTING THE ENTIRE SCRIPT
+// MAIN LOGIC
 // when dom content loaded is loaded
 //      load the interface with one simple button that says start
 //      when start button is clicked on, play rhythm.mp3 and guitar.mp3 at the same time
-//      
+
 
 // button click logic?:
 // create sort of like swimlanes for the following keys: a, s, j, k
@@ -17,9 +26,15 @@
 document.addEventListener('DOMContentLoaded', ()=> {
     // load interface that only has one button which says 'start'
     // when the start button is clicked on, call the createLanes function
-    start.addEventListener('onclick', createLanes)
-    // somehow play the background music
-    // somehow have the notes start falling down
+    start.addEventListener('onclick', () => {
+        createLanes()
+        // somehow play the background music
+        playBgMusic("master-of-puppets")
+        // somehow have the notes start falling down
+        playNotes(NOTES)
+
+        
+    })
     // somehow figure out how it's all gonna go down next 
 })
 
@@ -33,4 +48,16 @@ function createLanes() {
     // return something? do i need to save any info here? the center of each lane maybe?? 
     // like the exact horizontal place (each note area's center) where the key is supposed to fall
 
+}
+
+function playBgMusic(song) {
+    // somehow play the bg music
+    let guitar = new Audio('audios/master-of-puppets/guitar.mp3')
+    let rhythm = new Audio('audios/master-of-puppets/rhythm.mp3')
+    guitar.play()
+    rhythm.play()
+}
+
+function playNotes(notes) {
+    // somehow have the notes start falling down
 }
