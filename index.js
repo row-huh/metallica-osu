77a3f4be70a6350a249b97bda61d9221fb21f5ff
row-huh <rohaabropathan@gmail.com>
2025-04-01 13:40:30 +0500

    updated  index.html + created styles.css to test notes falling down animation

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 // necessary variables go here
-//NOTES = [   // all instances where the player must intervene. 
-//    {"time": 1.2, "key": "a", "sound" : "1.mp3"}
-//]  
+NOTES = [   // all instances where the player must intervene. 
+    {"time": 1.2, "key": "a"},
+    {"time": 2.2, "key": "s"},
+    {"time": 3.2, "key": "j"},
+    {"time": 4.2, "key": "k"}
+]
 
-//KEYS = ['a', 's', 'j', 'k']
+KEYS = ['a', 's', 'j', 'k']
 // NOTES attributes: Time is the time at which they should intervene, key is the action they must take, sound is the sound that will play when the provided action is taken
 
 
@@ -22,18 +25,24 @@
 // Also, assume any one hypothetical (or an exclusive html element, like a line) to be the boundary as in if the key
 // is hit at that boundary then it is a perfect hit - if not clicked upon then it must keep going down until it reaches
 // the end of the viewport - at which point, the key should disappear 
-
+// OVER
 
 
 document.addEventListener('DOMContentLoaded', ()=> {
     // load interface that only has one button which says 'start'
     // when the start button is clicked on, call the createLanes function
     start.addEventListener('onclick', () => {
-        createLanes(KEYS)
+        A = document.getElementById("key-container-a")
+        S = document.getElementById("key-container-s")
+        J = document.getElementById("key-container-j")
+        K = document.getElementById("key-container-k")
+
+        keys = {A, S, J, K}
+        // const lanes = createLanes(KEYS)
         // somehow play the background music
         playBgMusic("master-of-puppets")
         // somehow have the notes start falling down
-        playNotes(NOTES)
+        playNotes(NOTES, keys)
 
         
     })
@@ -43,21 +52,14 @@ document.addEventListener('DOMContentLoaded', ()=> {
 
 
 
+
+// i probably don't need this function 
 function createLanes(keys) {
-    // for each key in the array KEYS (a,s,d or whatever)
-    // get the total width of the viewport and then divide by the total length of array KEYS
-    // assign one partition for each key - this should somehow, im not yet sure how, but it should help
-    // in guiding each note where to fall
-    // return something? do i need to save any info here? the center of each lane maybe?? 
-    // like the exact horizontal place (each note area's center) where the key is supposed to fall
-    // return {key : "a", center: "difference from the left in pixels(int)"}??
-
-
+    // somehow create one container / div for each key in the array KEYS
 }
 
+
 export function playBgMusic(song) {
-    // somehow play the bg music
-    console.log("Oh look you have reached the function")
     let guitar = new Audio('../audios/master-of-puppets/guitar.mp3') // had to change path for test - might need to change again (remove ../)
     let rhythm = new Audio('../audios/master-of-puppets/rhythm.mp3')
     console.log("The music should start playing here")
@@ -67,12 +69,9 @@ export function playBgMusic(song) {
 
 
 
-function playNotes(notes) {
-    // somehow have the notes start falling down
-
-    // um i was thinking if there is any way that i start counting time
-    // and only when the current time matches the time attribute of the next element
-    // should the note be played?
-    // or maybe current time - delay matches the time attribute??
-    // because you have to deduct out the time it would take for it to fall?? gahhhhh idk
-}
+function playNotes(notes, lanes) {
+    // somehow, eaach note must be read 1 second before it must be executed
+    // that 1 sec is for the falling down animation
+    // The notes shouldn't go incrementally - like one goes through then we read the other
+    // Instead, notes should start appearing T minus 1 second, I think I need something asynchronous here
+    }
